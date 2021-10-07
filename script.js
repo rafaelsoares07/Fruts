ebec75eaf797f4f237f180fe76500c643dd41f53
Rafael Soares <rafaelsoares1936@gmail.com>
2021-10-07 13:58:30 -0300

    Adição das mudancças de cores do body e deixando o designer sem bordas(clean)

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
+function colocarImg1(srcImagem){
+let imagemPrincipal = document.querySelector('.imagem-fruta-principal')
+let corDeFundo = document.querySelector('body')
 
-let menu = document.querySelectorAll('.navegation li')
+imagemPrincipal.setAttribute("src",srcImagem)
+corDeFundo.setAttribute("class","vermelho")
+
+}
+
+function colocarImg2(srcImagem){
+let imagemPrincipal = document.querySelector('.imagem-fruta-principal')
+let corDeFundo = document.querySelector('body')
+
+imagemPrincipal.setAttribute("src",srcImagem)
+corDeFundo.setAttribute("class","amarelo")
+
+}
+
+
+function colocarImg3(srcImagem){
+let imagemPrincipal = document.querySelector('.imagem-fruta-principal')
+let corDeFundo = document.querySelector('body')
+
+imagemPrincipal.setAttribute("src",srcImagem)
+corDeFundo.setAttribute("class","amarelo")
+
+}
+
+
+function colocarImg4(srcImagem){
+let imagemPrincipal = document.querySelector('.imagem-fruta-principal')
+let corDeFundo = document.querySelector('body')
+
+imagemPrincipal.setAttribute("src",srcImagem)
+corDeFundo.setAttribute("class","roxo")
+
+}
+
+
+function colocarImg5(srcImagem){
+let imagemPrincipal = document.querySelector('.imagem-fruta-principal')
+let corDeFundo = document.querySelector('body')
+
+imagemPrincipal.setAttribute("src",srcImagem)
+corDeFundo.setAttribute("class","verde")
+
+}
 
 
 
